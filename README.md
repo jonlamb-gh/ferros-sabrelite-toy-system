ea1060e8332ada7f889db237de8729ddcff63e86
Jon Lamb <lamb.jon.io@gmail.com>
2021-10-23 05:42:25 -0700

    Basic example

---
+++ README.md
@@ -0,0 +1,10 @@
+
+See [QEMU sabrelite machine](https://qemu.readthedocs.io/en/latest/system/arm/sabrelite.html).
+
+```bash
+selfe build --platform sabre --sel4_arch aarch32
+```
+
+```bash
+selfe simulate --platform sabre --sel4_arch aarch32
+```
---
+++ cargo-build.sh
@@ -0,0 +1,18 @@
+set -e
+
+if [ -z ${SEL4_CONFIG_PATH+x} ]; then
+    echo "SEL4_CONFIG_PATH is unset; set it, or build with 'selfe'";
+    exit 1;
+fi
+
+if [ -z ${SEL4_PLATFORM+x} ]; then
+    echo "SEL4_PLATFORM is unset; set it, or build with 'selfe'";
+    exit 1;
+fi
+
+# build all packages in the right order, so binary packaging works as expected.
+echo "======================= building hello-printer ======================"
+cargo build -p hello-printer $@;
+
+echo "======================== building root-task ======================="
+cargo build -p root-task $@;
